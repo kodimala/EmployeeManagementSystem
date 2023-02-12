b9814222d52108f2dc0df9c8bd31b13c3ae3d18d
satyam <satyam1.thetruth@gmail.com>
2023-02-12 15:21:11 +0530

    first commit

---
+++ src/main/java/com/employeemanagement/EmployeesystemApplication.java
@@ -0,0 +1,19 @@
+package com.employeemanagement;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesystemApplication {
+	 private static final Logger logger = LoggerFactory.getLogger(EmployeesystemApplication.class);
+	   
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesystemApplication.class, args);
+		System.out.println("execution success");
+		logger.info("executing success wih logger");
+		
+	}
+
+}
